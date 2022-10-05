af5d1f43ade94975986dc47b85c741ecec3f36a0
Julie Lee <julielee9067@gmail.com>
2022-10-05 09:41:24 -0400

    Working example

--- core/stat_analyzer.py
+++ core/stat_analyzer.py
@@ -1,43 +1,79 @@
+from datetime import datetime
+from typing import Dict
+
 from sqlalchemy.orm import Session
 
 from core.config import engine
 from core.models import Country, CountryData
 
 import matplotlib
+import matplotlib.dates as mdates
+
+from core.utils import (
+    calculate_expected_num_deaths,
+    plot_graph_with_polyfit_line,
+)
 
 matplotlib.use("TkAgg")  # noqa
 
 import matplotlib.pyplot as plt  # noqa
 
 
-def plot_mortality_rate_by_date():
+def get_dates_and_num_deaths() -> Dict:
+    res = {}
     with Session(engine) as session:
         country_list = session.query(Country).join(CountryData).all()
         for country in country_list:
             dates = []
-            mortality_rates = []
+            num_deaths = []
 
             for data in country.country_datas:
                 if data.total_cases:
-
-                    # Ignore outlier
-                    if data.total_deaths / data.total_cases == 1:
-                        continue
-
-                    mortality_rates.append(data.total_deaths / data.total_cases * 100)
+                    expected_num_deaths = calculate_expected_num_deaths(
+                        total_cases=data.total_cases,
+                        total_deaths=data.total_deaths,
+                        population=country.population,
+                    )
+                    num_deaths.append(expected_num_deaths)
                     dates.append(data.collected_date)
 
-            if not mortality_rates:
+            if not num_deaths:  # Some total_deaths data were missing for a few countries
                 continue
 
-            plt.plot(dates, mortality_rates)
-            plt.text(dates[-1], mortality_rates[-1], country.country_code, fontdict={"size": 10})
+            res[country.country_code] = {"dates": dates, "num_deaths": num_deaths}
 
-    plt.xlabel("Date")
-    plt.ylabel("COVID-19 mortality rate")
-    plt.title("Date vs. COVID-19 Mortality Rate")
-    plt.savefig("resources/date_vs_mortality_rate.jpg")
+    return res
+
+
+def get_num_deaths_by_date(date: datetime):
+    res = dict()
+
+    with Session(engine) as session:
+        country_data_list = (
+            session.query(CountryData)
+            .filter(CountryData.collected_date == date)
+            .join(Country)
+            .all()
+        )
+        for data in country_data_list:
+
+            res[data.country.country_code] = calculate_expected_num_deaths(
+                total_deaths=data.total_deaths,
+                total_cases=data.total_cases,
+                population=data.country.population,
+            )
+
+    # Return in descending order with the highest num_deaths
+    return dict(sorted(res.items(), key=lambda item: item[1], reverse=True))
 
 
 if __name__ == "__main__":
-    plot_mortality_rate_by_date()
+    res = get_dates_and_num_deaths()
+    country_code = "MEX"
+    plot_graph_with_polyfit_line(
+        x=res[country_code]["dates"],
+        y=res[country_code]["num_deaths"],
+        title=f"date_vs_num_deaths_{country_code}",
+    )
+    numeric_dates = mdates.date2num(res[country_code]["dates"])
+    # print(get_mortality_rates_by_date(date=datetime(2021, 6, 30)))
---
+++ core/utils.py
@@ -0,0 +1,46 @@
+from datetime import date
+from typing import List, Any
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+
+from numpy import poly1d
+
+
+def calculate_expected_num_deaths(
+    total_cases: float, total_deaths: float, population: float
+) -> float:
+    return total_deaths / total_cases * population
+
+
+def get_polyfit_line(x: List[float], y: List[float], degree: int = 5) -> poly1d:
+    trend = np.polyfit(x, y, degree)
+    return np.poly1d(trend)
+
+
+def plot_graph_with_polyfit_line(
+    x: List,
+    y: List,
+    title: str,
+    x_label: str = "Date",
+    y_label: str = "Expected number of deaths by COVID-19",
+) -> None:
+    plt.plot(x, y)
+    numeric_x = mdates.date2num(x) if isinstance(x[0], date) else x
+    f = get_polyfit_line(numeric_x, y)
+    x_new = pd.date_range(start=x[0], end=x[-1], periods=100)
+    y_new = f(np.linspace(numeric_x[0], numeric_x[-1], 100))
+
+    plt.plot(x_new, y_new)
+    plt.xticks(fontsize=8)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig(f"resources/{title}.jpg")
+
+
+def predict_trend(input: Any, polyfit_line: poly1d) -> float:
+    numeric_input = mdates.date2num(input) if isinstance(input, date) else input
+    return polyfit_line(numeric_input)
